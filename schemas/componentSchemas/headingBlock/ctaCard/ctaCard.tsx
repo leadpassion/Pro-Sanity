2aac9bf18f9c8908eb98d98e37484968337b8cd5
leadpassion <janrickaguilar12@gmail.com>
2024-08-01 08:11:56 -0700

    fix: heading block

---
+++ schemas/componentSchemas/headingBlock/ctaCard/ctaCard.tsx
@@ -0,0 +1,68 @@
+import { defineInternalLinkField, defineLinkField } from '@/schemas/fields'
+import { PiArrowSquareRight } from 'react-icons/pi'
+import { defineField } from 'sanity'
+import { PreviewCtaCard } from './PreviewCtaCard'
+
+export const ctaCard = defineField({
+  name: 'ctaCard',
+  title: 'CTA Card',
+  type: 'object',
+  icon: PiArrowSquareRight,
+  fields: [
+    defineField({
+      name: 'leadInText',
+      title: 'Lead-In Text',
+      type: 'string',
+      initialValue: 'Have a more general question?',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'ctaText',
+      title: 'CTA Text',
+      type: 'string',
+      initialValue: 'Go to Contact Us page',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'linkType',
+      title: 'Link Type',
+      type: 'string',
+      options: {
+        list: [
+          { title: 'External', value: 'external' },
+          { title: 'Link to Content', value: 'internal' },
+        ],
+      },
+      initialValue: 'internal',
+    }),
+    defineLinkField({
+      name: 'externalLink',
+      title: 'External Link',
+      hidden: ({ parent }) => parent?.linkType !== 'external',
+    }),
+    defineInternalLinkField({
+      hidden: ({ parent }) => parent?.linkType !== 'internal',
+    }),
+  ],
+  preview: {
+    select: {
+      leadInText: 'leadInText',
+      ctaText: 'ctaText',
+      externalLink: 'externalLink',
+      internalLink: 'internalLink',
+    },
+    prepare({ leadInText, ctaText, externalLink, internalLink }) {
+      return {
+        title: 'CTA Card',
+        leadInText,
+        ctaText,
+        externalLink,
+        internalLink,
+      }
+    },
+  },
+  components: {
+    // @ts-expect-error
+    preview: PreviewCtaCard,
+  },
+})
